6f176f9cff851b6717d78b12ef1b8f9d4597f35f
fathy <fathyelnaa5@gmail.com>
2025-07-17 01:16:48 +0300

    clinic-up

--- Clinic-App/routes/web.php
+++ Clinic-App/routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('clinic.home');
+});
+Route::get('/history', function () {
+    return view('clinic.history');
+});
+Route::get('/contact', function () {
+    return view('clinic.contact');
+});
+Route::get('/login', function () {
+    return view('clinic.login');
+});
+Route::get('/majors', function () {
+    return view('clinic.majors');
+});
+Route::get('/register', function () {
+    return view('clinic.register');
+});
+Route::get('/doctor', function () {
+    return view('clinic.doctor');
+});
+Route::get('/doctor-home', function () {
+    return view('clinic.doctor-home');
 });
